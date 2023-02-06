ac810e9455b69f6bdc9b0dbbabe04e6456bb3ecc
narges-bagheri <111405499+narges-bagheri@users.noreply.github.com>
2023-02-06 11:03:59 +0330

    Add files via upload

---
+++ tamrin5.js
@@ -0,0 +1,41 @@
+//tamrin 1
+// const myArray=[]
+
+// let count=10;
+// for(let i=1;i<=count;i++){
+//     const temp=+prompt('enter a number : ')
+//     myArray.push(temp)
+// }
+// const smallest=Math.min(...myArray);
+// console.log('smallest number is : '+smallest);
+
+
+
+//tamri 2
+// const myArray=[]
+
+// let count=10;
+// for(let i=1;i<=count;i++){
+//     const temp=+prompt('enter a number : ')
+//     myArray.push(temp)
+// }
+// const largest=Math.max(...myArray);
+// console.log('largest number is : '+largest);
+
+
+//tamri 3
+// const myArray=[]
+// let count=10;
+// for(let i=1;i<=count;i++){
+//     const temp=+prompt('enter a number : ')
+//     myArray.push(temp)
+// }
+// let sum=0;
+// for (let j=0;j<myArray.length;j++){
+//     const m=myArray[j];
+//     sum=sum+m;
+   
+// }
+// console.log(sum);
+
+
